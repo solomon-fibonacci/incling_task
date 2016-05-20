3a0bf858f491275902890f0f05f4f7ad15613861
Solomon Omojola <solomonomojola@Solomons-MacBook-Pro.local>
2016-05-20 01:13:48 +0100

    Complete project

--- school/models.py
+++ school/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class School(models.Model):
+
+    school_name = models.CharField(max_length=100)
+    school_motto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.school_name
+
+
+class Classroom(models.Model):
+
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    classroom_title = models.CharField(max_length=100)
+    classroom_grade = models.IntegerField()
+
+    def __str__(self):
+        return "%s in %s" % (self.classroom_title, self.school)
+
+
+class Student(models.Model):
+
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    date_of_birth = models.DateField('student date of birth')
+
+    def __str__(self):
+        return "%s %s in %s" % (
+            self.first_name, self.last_name, self.classroom
+        )
--- school/views.py
+++ school/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 from time import strftime
 
+
 def index(request):
-    return HttpResponse(
-    	"<p align=\'center\''>Hey there! <br/> This would have been the landing page"
-    	+ " for the School Application, but because you're here at "
-    	+ strftime('%X %Z') 
-    	+ " you have arrived too early. <br/>"
-    	+ " You may be lucky if you come back in 13 seconds!</p>"
-    	)
+    response_string = ("<p align=\'center\''>Hey there! <br/> This would ",
+                       "have been the landing page for the",
+                       "School Application but because you're here at ",
+                       strftime('%X %Z'),
+                       " you have arrived too early. <br/>",
+                       " You may be lucky if you come back in 13 seconds!</p>"
+                       )
+    return HttpResponse(response_string)
